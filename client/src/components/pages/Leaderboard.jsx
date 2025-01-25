acf6bb321d5406eca83fec458a2998f56aeaba08
saisnehag99 <saisnehag99@gmail.com>
2025-01-25 01:47:43 -0600

    sort leaderboard

--- client/src/components/pages/Leaderboard.jsx
+++ client/src/components/pages/Leaderboard.jsx
@@ -13,6 +13,11 @@ const Leaderboard = () => {
   const scores = state?.scores || {};
   const socketToUserMap = state?.socketToUserMap || {};
 
+  // Sort socketToUserMap by score
+  const entries = Object.entries(socketToUserMap);
+  entries.sort((a, b) => scores[b[0]] - scores[a[0]]);
+  const sortedSocketToUserMap = new Map(entries);
+
   return (
     <div className="leaderboard-page-container">
       {/* <Header backNav="" />   */}
@@ -21,12 +26,13 @@ const Leaderboard = () => {
           <p className="font-medium place-self-center text-2xl pt-3 pb-1">Leaderboard</p>
           <hr class="border-[#675325] border-[0.5pt] mx-5"></hr>
           <div className="space-y-2 font-normal text-xl mt-4 mx-2">
-            {Object.entries(socketToUserMap).map(([playerId, player]) => (
-              <div key={playerId} className="flex justify-between px-5">
-                <span>{player.name}</span>
-                <span className="font-bold">{scores[playerId] || 0} points</span>
-              </div>
-            ))}
+            {Array.from(sortedSocketToUserMap)
+              .map(([playerId, player]) => (
+                <div key={playerId} className="flex justify-between px-5">
+                  <span>{player.name}</span>
+                  <span className="font-bold">{scores[playerId] || 0} points</span>
+                </div>
+              ))}
           </div>
         </div>
       </div>
