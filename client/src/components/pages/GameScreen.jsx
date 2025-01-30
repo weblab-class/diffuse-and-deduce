a3ff18d10d5a054177391e204270cd46f6ca5ef0
Peter Lin <plin503@mit.edu>
2025-01-30 00:29:59 -0500

    fixed resetting selected player in sabotage after action

--- client/src/components/pages/GameScreen.jsx
+++ client/src/components/pages/GameScreen.jsx
@@ -105,7 +105,7 @@ export default function GameScreen() {
   const performSabotage = useCallback(
     (type) => {
       if (!canSabotage) {
-        alert("You can perform sabotage actions once every 30 seconds.");
+        alert("You can only perform sabotage actions once every 30 seconds.");
         return;
       }
 
@@ -154,6 +154,8 @@ export default function GameScreen() {
         default:
           break;
       }
+
+      setSelectedOpponent(null);
     };
 
     window.addEventListener("keydown", handleKeyDown);
